26334509e008f3798d849b019c0f3c4ae25616d0
enzo <enzosouza20077@gmail.com>
2025-03-21 09:49:54 -0300

    js e css

--- js/carousel.js
+++ js/carousel.js
@@ -2,22 +2,22 @@
 
 //carousel
 
-//Array storage class
+//Array storage class(armazena as imagens do carrosel)
 let carouselArr = [];
 
-//class Carousel("carousel" kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk)
+//class Carousel("carousel" kkkkkkkkkkkkkkkkkkk)(modelo para representar cada imagem do carrosel)
 class Carousel {
     static Start(arr) {
-        if (arr && arr.length > 0) {
-            Carousel._sequence = 0;
-            Carousel._size = arr.length;
+        if (arr && arr.length > 0) {//verifica se tem imagens no carrosel
+            Carousel._sequence = 0;//indica que começa da primeira imagem
+            Carousel._size = arr.length;//define qual o tamanho dependendo das imagens que eu vou colocar 
             Carousel.Next(); // Iniciar carrossel
             Carousel._interval = setInterval(function () {
-                Carousel.Next();
-            }, 1500);//era pra ser 5000 mas tava demorando mt
-        } else {
-            throw "Method Start need a Array Variable.";
-        }
+                Carousel.Next();//exibe a primeira imagem antes da troca e quanto tempo pra trocar
+            }, 1500);//era pra ser 5000 mas tava demorando mt(perguntar pra atila ou pra juarez quanto tempo devo deixar 5000 é sacanagem)
+            } else {
+                throw "Method Start need a Array Variable.";//acho que é uma mensagem de erro(lembrete:TRADUZIR OU MUDAR A MENSAGEM.)
+            }
     }
     static Next() {
         ///contêiner do carrossel
@@ -28,12 +28,12 @@ class Carousel {
             // pega a imagem atual dos carros
             let currentImage = carouselArr[Carousel._sequence];
 
-            // Atualiza a imagem dos carros(obgd youtube)
+            // Atualiza a imagem dos carros como uma imagem redirecionavel(obgd youtube)
             carouselElement.innerHTML = `<a href="${currentImage.link}">
-                <img src="img/${currentImage.img}" width="500"
-            </a>`;
+                <img src="img/${currentImage.img}" width="730"
+            </a>`
 
-            // coloca outro titulo
+            // coloca outro titulo na pagina dos carros
             titleElement.innerHTML = `<p>${currentImage.text}</p>`;
 
             // Muda para a próxima imagem(obgdchat pela ajuda)
